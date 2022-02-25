017758f31fc6bd2f4351b184c735530d3b641e92
arorarmr <arora.rmr@gmail.com>
2022-02-25 17:09:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -131,7 +131,7 @@ st.markdown('''
 ## Work Experience
 ''')
 
-txt('**Bixby Voice Assistant Analytics**, "Data Scientist, Samsung Research, Bangalore, India",
+txt('**Bixby Voice Assistant Analytics**', 'Data Scientist, Samsung Research, Bangalore, India',
 '2020-Present')
 st.markdown('''
 
@@ -142,7 +142,7 @@ st.markdown('''
 - Conducted several organizational and team level trainings on data structures for internal coding challenges
 ''')
 
-txt('**Non-Invasive Glucose Monitoring**, "Data Scientist, Samsung Research, Bangalore, India",
+txt('**Non-Invasive Glucose Monitoring**', 'Data Scientist, Samsung Research, Bangalore, India',
 '2018-2020')
 st.markdown('''
 - Developed several algorithms for predicting glucose information from NIR and PPG signals
@@ -151,7 +151,7 @@ st.markdown('''
 - Filed patents and papers based on the algorithms developed
 ''')
 
-txt('**OpenCL APIs Implementation For GPU**, "Lead Software Developer, Samsung Research, Bangalore, India",
+txt('**OpenCL APIs Implementation For GPU**', 'Lead Software Developer, Samsung Research, Bangalore, India',
 '2016-2018')
 st.markdown('''
 - Implemented OpenCL APIs for Samsung GPU according to OpenCL 1.2 specification by Khronos
@@ -159,14 +159,14 @@ st.markdown('''
 - Fixed several bugs for already implemented API’s using C++ language
 ''')
 
-txt('**Performance Improvement using OpenCL and OpenGL**, "Senior Software Developer, Samsung Research, Bangalore, India",
+txt('**Performance Improvement using OpenCL and OpenGL**', 'Senior Software Developer, Samsung Research, Bangalore, India',
 '2014-2016')
 st.markdown('''
 - Created OpenCL kernels for SKIA API’s (2D graphics library for android), Low-Light Imaging and Kids-Mode Application. The solution worked for both MALI and QCOM GPUs
 - Fish eye images were converted and stitched together using OpenGL framework to create a single 360 images for Samsung camera devices
 ''')
 
-txt('**Crypt Analysis using hybrid computing**, "Software Developer, C-DAC R&D, Bangalore, India",
+txt('**Crypt Analysis using hybrid computing**', 'Software Developer, C-DAC R&D, Bangalore, India',
 '2012-2014')
 st.markdown('''
 - Retrieved metadata information from encrypted files such as Winzip and MS Word files
