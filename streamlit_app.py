d2893e5994af4f11251562ee293075ea072aa815
arorarmr <arora.rmr@gmail.com>
2022-02-25 17:25:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ st.markdown("""
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="https://share.streamlit.io/arorarmr/my_resume/main">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#summary">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#education">Education</a>
