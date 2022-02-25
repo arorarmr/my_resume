94f59c3b878b8c26a52d470cf66d9c9209f3cb11
arorarmr <arora.rmr@gmail.com>
2022-02-25 16:55:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,13 +81,20 @@ def txt3(a, b):
     st.markdown(b)
   
 def txt4(a, b, c):
-  col1, col2, col3 = st.columns([1,3,0.25])
+  col1, col2, col3 = st.columns([1,3,1])
   with col1:
     st.markdown(f'`{a}`')
   with col2:
     st.markdown(b)
   with col3:
     st.markdown(c)
+    
+def txt4(a, b):
+    col1, col2 = st.columns([1,4])
+    with col1:
+        st.markdown(f'`{a}`')
+    with col2:
+        st.markdown(b)
 
 #####################
 st.markdown('''
@@ -181,9 +188,9 @@ txt4('Patent','Method and system for predicting blood compound concentration of 
 txt4('Research Paper','Motion artifact correction for photoplethysmogram (PPG) using advanced wavelet-based method','https://ieeexplore.ieee.org/abstract/document/8857131')
 txt4('Research Poster','Motion artifact removal method for photoplethysmogram (PPG) signal','EMBC 2019')
 
-txt4('Award','Spot Award for relentless effort and contribution in glucose monitoring project','')
-txt4('Award','Employee of the Month Award for quick learning and fast project delivery','')
-txt4('Award','C-DAC Best Student Award for best performance among all students','')
+txt5('Award','Spot Award for relentless effort and contribution in glucose monitoring project')
+txt5('Award','Employee of the Month Award for quick learning and fast project delivery')
+txt5('Award','C-DAC Best Student Award for best performance among all students')
 
 
 #####################
