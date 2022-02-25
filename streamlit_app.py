ed00dde2e79e6ca34fedadf85a8b7d2750d8be64
arorarmr <arora.rmr@gmail.com>
2022-02-25 17:13:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,14 @@ st.markdown("""
 
 #####################
 # Custom function for printing text
-def txt(a, b, c):
+def txt(a, b):
+  col1, col2 = st.columns([5,1])
+  with col1:
+    st.markdown(a)
+  with col2:
+    st.markdown(c)
+
+def txt1(a, b, c):
   col1, col2 = st.columns([5,1])
   with col1:
     st.markdown(a)
@@ -103,7 +110,7 @@ st.markdown('''
 ''')
 
 txt('**PG Diploma in Advanced Computing**, C-DAC Bangalore',
-'2011-2012')
+'2011-2012',)
 st.markdown('''
 - Percentage: `73`
 ''')
@@ -131,7 +138,7 @@ st.markdown('''
 ## Work Experience
 ''')
 
-txt('**Bixby Voice Assistant Analytics**', 'Data Scientist, Samsung Research, Bangalore, India',
+txt1('**Bixby Voice Assistant Analytics**', 'Data Scientist, Samsung Research, Bangalore, India',
 '2020-Present')
 st.markdown('''
 
@@ -142,7 +149,7 @@ st.markdown('''
 - Conducted several organizational and team level trainings on data structures for internal coding challenges
 ''')
 
-txt('**Non-Invasive Glucose Monitoring**', 'Data Scientist, Samsung Research, Bangalore, India',
+txt1('**Non-Invasive Glucose Monitoring**', 'Data Scientist, Samsung Research, Bangalore, India',
 '2018-2020')
 st.markdown('''
 - Developed several algorithms for predicting glucose information from NIR and PPG signals
@@ -151,7 +158,7 @@ st.markdown('''
 - Filed patents and papers based on the algorithms developed
 ''')
 
-txt('**OpenCL APIs Implementation For GPU**', 'Lead Software Developer, Samsung Research, Bangalore, India',
+txt1('**OpenCL APIs Implementation For GPU**', 'Lead Software Developer, Samsung Research, Bangalore, India',
 '2016-2018')
 st.markdown('''
 - Implemented OpenCL APIs for Samsung GPU according to OpenCL 1.2 specification by Khronos
@@ -159,14 +166,14 @@ st.markdown('''
 - Fixed several bugs for already implemented API’s using C++ language
 ''')
 
-txt('**Performance Improvement using OpenCL and OpenGL**', 'Senior Software Developer, Samsung Research, Bangalore, India',
+txt1('**Performance Improvement using OpenCL and OpenGL**', 'Senior Software Developer, Samsung Research, Bangalore, India',
 '2014-2016')
 st.markdown('''
 - Created OpenCL kernels for SKIA API’s (2D graphics library for android), Low-Light Imaging and Kids-Mode Application. The solution worked for both MALI and QCOM GPUs
 - Fish eye images were converted and stitched together using OpenGL framework to create a single 360 images for Samsung camera devices
 ''')
 
-txt('**Crypt Analysis using hybrid computing**', 'Software Developer, C-DAC R&D, Bangalore, India',
+txt1('**Crypt Analysis using hybrid computing**', 'Software Developer, C-DAC R&D, Bangalore, India',
 '2012-2014')
 st.markdown('''
 - Retrieved metadata information from encrypted files such as Winzip and MS Word files
